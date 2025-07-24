e3ab47edcbbd6fd7db45b0c2388f5dd665c24f30
IlyaBashlyaev <ibashlyaev2000@gmail.com>
2025-07-24 13:45:14 +0200

    Init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Website Blocker
+
+The small and efficient Userscript file that blocks certain websites. This file can be used in the browser extensions, such as Tampermonkey.
+
+The only two things that have to be done are pasting in Tampermonkey and adding websites. To add blocked websites, replace the string in line 14 and paste domains or URLs that are separated by a line break (⏎).
+
+The blocked websites list can look, for example, like this:
+```
+const blocked_websites = `
+tiktok.com
+youtube.com/shorts
+`
+```
+
+![Website Blocker](images/Website_Blocker.jpg)
---
+++ website_blocker.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         Website Blocker
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Blocks certain websites
+// @author       Ilya Bashlyaev
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+// Blocked websites list: domains and their subdomains, full website URLs
+// Edit this list to enter your websites
+const blocked_websites = `
+--- Replace this string and enter your websites here ---
+`;
+
+(function () {
+    'use strict';
+    // Getting the blocked websites from the custom function
+    const blocked_websites_array = blocked_websites.split('\n').filter(i => i);
+    console.log(blocked_websites_array);
+
+    // Getting the current domain and URL
+    const current_domain = window.location.hostname,
+        current_URL = window.location.href;
+
+    // Fast website check
+    const is_blocked = blocked_websites_array.some(website => current_domain.endsWith(website) || current_URL.includes(website));
+
+    if (is_blocked) {
+        try {
+            // Display of the blocking message instead of the page content
+            document.documentElement.innerHTML = `
+                <style>
+                    body {
+                        margin: 0;
+                        padding: 2em;
+                        height: calc(100vh - 4em);
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        text-align: center;
+                        font-family: sans-serif;
+                        background-color: #111;
+                        color: #fff;
+                    }
+                    .block-message {
+                        max-width: 600px;
+                    }
+                </style>
+                <body>
+                    <div class="block-message">
+                        <h1>🚫 Website blocked 🚫</h1>
+                        <p>This webpage has been blocked by the Userscript called "Website Blocker".</p>
+                        <p>If you believe that this is an error, update the blocked websites list in your Userscript.</p>
+                    </div>
+                </body>
+            `;
+        }
+        catch {
+            // Redirect to Google if there was error on replacing the content
+            window.location.href = 'https://www.google.com';
+        }
+
+        // Stop further webpage execution
+        throw new Error('Access to this webpage is blocked');
+    }
+})();
